127a8bb8a5ae2cb1d1ea26ea6bafa44602740cd1
Alex-Mercy <hambiru@yandex.ru>
2022-02-16 22:27:55 +0300

    Updates

---
+++ precache-manifest.75c6797ab200f4ac98917cdd5567a113.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f3884a1e745102ce80176ea94297ff9",
+    "url": "/MUI-blog/index.html"
+  },
+  {
+    "revision": "b1a392a26789d5ad7d65",
+    "url": "/MUI-blog/static/js/2.0a38679a.chunk.js"
+  },
+  {
+    "revision": "def5d103674016bfdd08",
+    "url": "/MUI-blog/static/js/main.263971d8.chunk.js"
+  },
+  {
+    "revision": "2e8b4402462e17e6f46f",
+    "url": "/MUI-blog/static/js/runtime-main.9bfc4818.js"
+  }
+]);
